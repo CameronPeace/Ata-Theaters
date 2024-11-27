9f3c0814d8dc63d75469d32594bd9ca880dacb48
Cameron Peace <cameronpeadev@gmail.com>
2024-11-27 17:31:35 -0500

    try breeze

---
+++ resources/js/components/App.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+export const App = () => {
+
+    return (
+        <div>
+            <h1>Users list</h1>
+            <ul>
+            </ul>
+        </div>
+    );
+};
